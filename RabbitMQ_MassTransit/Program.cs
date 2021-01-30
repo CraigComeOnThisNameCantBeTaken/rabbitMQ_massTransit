745eac09417300b770d8fb426ed5b911598bfffd
Craig Freeman <freema48@gmail.com>
2021-01-30 23:33:12 +0000

    basic producer and consumer

---
+++ RabbitMQ_MassTransit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using RabbitMQ.Client;
+
+namespace RabbitMQ_MassTransit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Receiver.Listen();
+            Sender.Send();
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
